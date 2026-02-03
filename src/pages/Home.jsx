1fd0f81ded0b66d69489d6d514020132990e5c37
Fahim <fahimshareear@gmail.com>
2026-02-03 20:43:37 +0600

    adding brands swiper in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/shared/Banner';
 // import LatestTuitions from '../components/shared/LatestTuitions';
 import HowItWorks from '../components/HowItWorks';
 import StatsCounter from '../components/Stats';
+import Brands from '../components/Brands';
 // import PopularTutors from '../components/Tutors';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <StatsCounter></StatsCounter>
             <HowItWorks></HowItWorks>
+            <Brands></Brands>
         </div>
     );
 };
